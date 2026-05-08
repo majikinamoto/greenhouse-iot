b25102f5a4c2040bd7d88ea4604de21f7b28b76c
majikinamoto <majikina.moto@gmail.com>
2026-05-08 14:04:36 +0900

    5.8

--- download_csv.php
+++ download_csv.php
@@ -31,23 +31,14 @@ echo "\xEF\xBB\xBF";
 
 $output = fopen('php://output', 'w');
 
-fputcsv($output, [
-    '日時',
-    'user_id',
-    'point_id',
-    '温度',
-    '湿度',
-    'CO2',
-    '日射',
-    '電圧',
-    '飽和水蒸気量',
-    '水蒸気量',
-    '飽差'
-]);
+function calcVapor($temp, $hum) {
+    $es = 6.1078 * pow(10, (7.5 * $temp) / (237.3 + $temp));
+    $ea = $es * $hum / 100;
+    return 216.7 * ($ea / ($temp + 273.15));
+}
 
 $sql = "SELECT
             DATE_FORMAT(recorded_at, '%Y-%m-%d %H:%i:%s') AS recorded_at,
-            user_id,
             point_id,
             temperature,
             humidity,
@@ -57,7 +48,7 @@ $sql = "SELECT
         FROM measurements
         WHERE user_id = ?
           AND recorded_at BETWEEN ? AND ?
-          ORDER BY point_id ASC, recorded_at ASC";
+        ORDER BY recorded_at ASC, point_id ASC";
 
 $stmt = $conn->prepare($sql);
 
@@ -69,38 +60,92 @@ $stmt->bind_param("sss", $user_id, $start_dt, $end_dt);
 $stmt->execute();
 $result = $stmt->get_result();
 
+$rows = [];
+
 while ($row = $result->fetch_assoc()) {
 
-    $sat = "";
-    $vapor = "";
-    $vpd = "";
+    $time = $row["recorded_at"];
+    $point = $row["point_id"];
 
-    if ($row["temperature"] !== null && $row["humidity"] !== null) {
-
-        $temp = floatval($row["temperature"]);
-        $hum  = floatval($row["humidity"]);
-
-        $es = 6.1078 * pow(10, (7.5 * $temp) / (237.3 + $temp));
-        $ea = $es * $hum / 100;
-
-        $sat = 216.7 * ($es / ($temp + 273.15));
-        $vapor = 216.7 * ($ea / ($temp + 273.15));
-        $vpd = $sat - $vapor;
+    if (!isset($rows[$time])) {
+        $rows[$time] = [
+            "日時" => $time,
+            "P01_温度" => "",
+            "P01_湿度" => "",
+            "P01_飽和水蒸気量" => "",
+            "P01_水蒸気量" => "",
+            "P01_飽差" => "",
+            "P02_温度" => "",
+            "P02_湿度" => "",
+            "P11_温度" => "",
+            "P11_湿度" => "",
+            "P21_CO2" => "",
+            "P31_日射" => "",
+            "P91_電圧" => ""
+        ];
     }
 
-    fputcsv($output, [
-        $row["recorded_at"],
-        $row["user_id"],
-        $row["point_id"],
-        $row["temperature"],
-        $row["humidity"],
-        $row["CO2"],
-        $row["solar_radiation"],
-        $row["voltage"],
-        $sat === "" ? "" : round($sat, 3),
-        $vapor === "" ? "" : round($vapor, 3),
-        $vpd === "" ? "" : round($vpd, 3)
-    ]);
+    if ($point === "P01") {
+        $rows[$time]["P01_温度"] = $row["temperature"];
+        $rows[$time]["P01_湿度"] = $row["humidity"];
+
+        if ($row["temperature"] !== null && $row["humidity"] !== null) {
+            $temp = floatval($row["temperature"]);
+            $hum  = floatval($row["humidity"]);
+
+            $sat = calcVapor($temp, 100);
+            $vapor = calcVapor($temp, $hum);
+            $vpd = $sat - $vapor;
+
+            $rows[$time]["P01_飽和水蒸気量"] = round($sat, 3);
+            $rows[$time]["P01_水蒸気量"] = round($vapor, 3);
+            $rows[$time]["P01_飽差"] = round($vpd, 3);
+        }
+    }
+
+    if ($point === "P02") {
+        $rows[$time]["P02_温度"] = $row["temperature"];
+        $rows[$time]["P02_湿度"] = $row["humidity"];
+    }
+
+    if ($point === "P11") {
+        $rows[$time]["P11_温度"] = $row["temperature"];
+        $rows[$time]["P11_湿度"] = $row["humidity"];
+    }
+
+    if ($point === "P21") {
+        $rows[$time]["P21_CO2"] = $row["CO2"];
+    }
+
+    if ($point === "P31") {
+        $rows[$time]["P31_日射"] = $row["solar_radiation"];
+    }
+
+    if ($point === "P91") {
+        $rows[$time]["P91_電圧"] = $row["voltage"];
+    }
+}
+
+$header = [
+    "日時",
+    "P01_温度",
+    "P01_湿度",
+    "P01_飽和水蒸気量",
+    "P01_水蒸気量",
+    "P01_飽差",
+    "P02_温度",
+    "P02_湿度",
+    "P11_温度",
+    "P11_湿度",
+    "P21_CO2",
+    "P31_日射",
+    "P91_電圧"
+];
+
+fputcsv($output, $header);
+
+foreach ($rows as $row) {
+    fputcsv($output, $row);
 }
 
 fclose($output);
